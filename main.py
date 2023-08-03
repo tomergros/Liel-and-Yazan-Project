4f3c3bc60bee2f7e3794334200c315ae101d7af6
tomergros <77513087+tomergros@users.noreply.github.com>
2023-08-03 18:56:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,12 @@ st.markdown(
             background-color: #f0f0f0; /* Even lighter gray */
             padding: 20px;
         }
-        h1, h2, h3, h4, h5, h6, p {
+        h1, h2, h3, h4, h5, h6{
             text-align: center;
             font-weight: bold;
+        }
+        p{
+            text-align: center;
         }
     </style>
     """,
