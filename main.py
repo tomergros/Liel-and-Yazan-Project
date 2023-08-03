fbdc443ca3f52491d7c6c039cd0036b671493a2d
tomergros <77513087+tomergros@users.noreply.github.com>
2023-08-03 16:19:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,55 @@
 import streamlit as st
 
+# Center alignment HTML tag
+center_alignment = '<div style="text-align: center;">{}</div>'
+
 # Section 1
-st.header('זיהוי בריונות ברשתות החברתיות באמצעות בינה מלאכותית')
+st.markdown(center_alignment.format('<h1>זיהוי בריונות ברשתות החברתיות באמצעות בינה מלאכותית</h1>'))
 
 st.subheader('יזן מרעי וליאל יעקב')
 
 # Section 2
-st.header('רקע')
+st.markdown(center_alignment.format('<h2>רקע</h2>'))
 
 background_text = """
 בשנים האחרונות פלטפורמת הרשתות החברתיות תפסה תאוצה וכיום היא תופסת מקום משמעותי בחיים של כמעט כל אחד מאיתנו. 
-הרשתות החברתיות מספקות דרך קלה ונוחה ליצירת קשר ושיתוף עם הסביבה שלנו וגם עם אנשים זרים מכל רחבי העולם. 
-למעשה, כיום התקשורת המרכזית עם הסביבה שלנו מנוהלת דרך הרשתות החברתיות.
+הרשתות החברתיות מספקות דרך קלה ונוחה ליצירת קשר ושיתוף עם הסביבה שלנו וגם עם אנשים זרים מכל רחבי העולם. למעשה, 
+כיום התקשורת המרכזית עם הסביבה שלנו מנוהלת דרך הרשתות החברתיות.
 
 עם זאת, ולמרות היתרונות שבדבר, קיים גם חיסרון לא מבוטל- בריונות. בריונות ברשת היא הטרדה או העלבת אדם על ידי שליחת הודעה, 
-פרסום פוסט, תמונה או סירטון בעלי אופי פוגע או מאיים. תופעה זו משפיעה על כל המשתמשים ברשתות. 
-ועלולה לגרום לנזק לבריאות הנפשית והפיזית של הקורבנות.
+פרסום פוסט, תמונה או סירטון בעלי אופי פוגע או מאיים. תופעה זו משפיעה על כל המשתמשים ברשתות. ועלולה לגרום לנזק לבריאות הנפשית 
+והפיזית של הקורבנות.
 """
 
-st.write(background_text)
+st.markdown(center_alignment.format(background_text))
 
 # Section 3
-st.header('מטרות')
+st.markdown(center_alignment.format('<h2>טרות</h2>'))
 
 objectives_text = """
 מטרת המחקר שלנו היא לאתר טקסטים פוגעניים ברשתות החברתיות על מנת למנוע מצבים של פגיעה במשתמשים ברשתות החברתיות.
 נעשה זאת באמצעות בינה מלאכותית.
 
 קיימות הצעות רבות אשר צלחו את האתגר בשפות שנות ובעיקרן השפה האנגלית.
-לאחר שהצלחנו לקבל תוצאות טובות בשפה האנגלית, אנו עובדים על אלגוריתם המבוסס על מודל Multilingual Bert אשר יוכל לנתח 
-טקסטים בשפות עברית וערבית.
+לאחר שהצלחנו לקבל תוצאות טובות בשפה האנגלית, אנו עובדים על אלגוריתם המבוסס על מודל Multilingual Bert אשר יוכל לנתח טקסטים 
+בשפות עברית וערבית.
 
 בנוסף, אנו עובדים על כריית נתונים בשתי השפות הללו. הנתונים נלקחים מהרשת החברתית פייסבוק, על מנת ליצור סט נתונים המשלב 
 את שתי השפות.
 
 שפות אלו נחשבות קשות יותר לביצוע עיבוד טקסט ובנוסף, הפתרונות המוצעים בשפות אלו מעטים מאוד.
 
-בכוונתנו להצליח, לאחר אימון והשגת הערכות ביצוע טובות, לבנות פונקציה אשר תקבל משפט חדש ותחזיר את ההסתברות של 
-השייכות שלו לכל אחת מהקטגוריות. המטרה המרכזית היא שהמודל יפעל על השפות עברית וערבית יחדיו.
+בכוונתנו להצליח, לאחר אימון והשגת הערכות ביצוע טובות, לבנות פונקציה אשר תקבל משפט חדש ותחזיר את ההסתברות של השייכות 
+שלו לכל אחת מהקטגוריות. המטרה המרכזית היא שהמודל יפעל על השפות עברית וערבית יחדיו.
 """
 
-st.write(objectives_text)
+st.markdown(center_alignment.format(objectives_text))
 
 # Section 4
-st.header('NLP MODELS')
+st.markdown(center_alignment.format('<h2>NLP MODELS</h2>'))
 
 nlp_models_text = """
-תחילה, ערכנו ניסוי לקביעת האלגוריתם המספק את התוצאות הכי טובות בין מספר מודלים שונים.
+תחילה, ערכנו ניסוי לקביעת האלגוריתם המספק את התוצאות הכי טובות בין מספר מודלים שונים.  
 
 ביצענו השוואה בין שלושה אלגוריתמי סיווג: Naive bayes, LSTM ו- BERT.
 
@@ -55,15 +58,12 @@ nlp_models_text = """
 סט הנתונים מכיל 7,945 תגובות לא פוגעניות ו-16,250 תגובות פוגעניות מקטגוריות שונות בשפה האנגלית.
 
 נוכחנו לגלות שמודל BERT מספק את התוצאות הטובות ביותר.
-
-תוצאות סיווג המודלים
-Naive Bayes                                 BERT                                LSTM 
 """
 
-st.write(nlp_models_text)
+st.markdown(center_alignment.format(nlp_models_text))
 
 # Section 5
-st.header('Multilingual Bert')
+st.markdown(center_alignment.format('<h2>Multilingual Bert</h2>'))
 
 multilingual_bert_text = """
 לאור התוצאות, בחרנו להשתמש לסט הנתונים בעברית וערבית במודל Multilingual Bert.
@@ -72,33 +72,24 @@ multilingual_bert_text = """
 
 המודל אומן על מאגר נתונים בשפות שונות. למודל זה מבנה דומה למודל BERT והוא מורכב משני שלבים עיקריים:
 
-- Masked word אשר עוזר ללמידת קשרים בין מילים ו- Transformer layers על מנת ליצור ייצוגים למילים.
+Masked word אשר עוזר ללמידת קשרים בין מילים ו- Transformer layers על מנת ליצור ייצוגים למילים.
 
 מודל זה, משמש למגוון משימות בתחום עיבוד השפה, בין היתר ניתוח רגש של טקסטים בשפות שונות.
 """
 
-st.write(multilingual_bert_text)
+st.markdown(center_alignment.format(multilingual_bert_text))
 
 # Section 6
-st.header('דוגמא מתוך סט הנתונים בעברית וערבית בנפרד')
+st.markdown(center_alignment.format('<h2>דוגמא מתוך סט הנתונים בעברית וערבית בנפרד</h2>'))
 
 example_data_text = """
-סט הנתונים
-את הנתונים אנו אוספים מהרשת החברתית פייסבוק.
-
-אנו ממשיכים לאסוף נתונים בעבודה עצמית ומשתדלים לשמור על איזון בין שתי הקטגוריות של תגובות פוגעניות ושאינן פוגעניות.
-
-כמו כן, אנו מגוונים את הנתונים הנאספים מהרשת החברתית פייסבוק ומגוונים את סוגי הבריונות, למשל: פוליטיקה, גזענות, דת, מין וכו' 
-על מנת ליצור סט נתונים אותנטי ורחב ככל שנצליח.
-
-בכוונתנו להוסיף מקורות לאיסוף הנתונים כגון: אינסטגרם וטיקטוק. כידוע, ברשתות אלו שימושם של בני הנוער גדול משימושם 
-בפייסבוק. לכן, בכדי שסט הנתונים יהיה עדכני, רלוונטי ותואם לבני הנוער- נאסוף נתונים גם משם.
+...
 """
 
-st.write(example_data_text)
+st.markdown(center_alignment.format(example_data_text))
 
 # Section 7
-st.header('מדדים להערכות ביצועים')
+st.markdown(center_alignment.format('<h2>מדדים להערכות ביצועים</h2>'))
 
 performance_metrics_text = """
 את הערכות הביצועים שהתקבלו נשווה באמצעות שני מדדים: F1 score- זהו מדד לניתוח סטטיסטי של סיווג בינארי. 
@@ -112,31 +103,32 @@ performance_metrics_text = """
 - Accuracy (דיוק)- זהו המדד הפשוט ביותר. הוא מחשב את היחס בין הסיווגים הנכונים לבין סך כל הסיווגים.
 """
 
-st.write(performance_metrics_text)
+
+st.markdown(center_alignment.format(performance_metrics_text))
 
 # Section 8
-st.header('הערכת ביצועים על סט הנתונים בעברית')
+st.markdown(center_alignment.format('<h2>הערכת ביצועים על סט הנתונים בעברית</h2>'))
 
 hebrew_performance_text = """
-[Insert the performance results for the Hebrew data here]
+...
 """
 
-st.write(hebrew_performance_text)
+st.markdown(center_alignment.format(hebrew_performance_text))
 
 # Section 9
-st.header('הערכת ביצועים על סט הנתונים בערבית')
+st.markdown(center_alignment.format('<h2>הערכת ביצועים על סט הנתונים בערבית</h2>'))
 
 arabic_performance_text = """
-[Insert the performance results for the Arabic data here]
+...
 """
 
-st.write(arabic_performance_text)
+st.markdown(center_alignment.format(arabic_performance_text))
 
 # Section 10
-st.header('הערכת ביצועים על סט הנתונים בעברית + ערבית')
+st.markdown(center_alignment.format('<h2>הערכת ביצועים על סט הנתונים בעברית + ערבית</h2>'))
 
 hebrew_arabic_performance_text = """
-[Insert the performance results for the Hebrew + Arabic data here]
+...
 """
 
-st.write(hebrew_arabic_performance_text)
+st.markdown(center_alignment.format(hebrew_arabic_performance_text))
